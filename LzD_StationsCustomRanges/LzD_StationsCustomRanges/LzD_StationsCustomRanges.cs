a0ac23a766e735c3d2b2c268abf73ebc96102986
ilzd <ianlouzad4@gmail.com>
2023-12-21 16:20:56 -0300

    new settings texts

--- LzD_StationsCustomRanges/LzD_StationsCustomRanges/LzD_StationsCustomRanges.cs
+++ LzD_StationsCustomRanges/LzD_StationsCustomRanges/LzD_StationsCustomRanges.cs
@@ -61,38 +61,38 @@ namespace LzD_StationsCustomRanges
 
         void Awake()
         {
-            modEnabled = Config.Bind<bool>("1 - General", "a. Mod", true, "Enable or disable the mod completely");
-            logsEnabled = Config.Bind<bool>("1 - General", "b. Logs", false, "Enable or disable logs completely");
-            rangeEnabled = Config.Bind<bool>("1 - General", "c. Base range modifications", true, "Enable or disable base range customizations completely");
-            rangeLvlEnabled = Config.Bind<bool>("1 - General", "d. Range per level modifications", true, "Enable or disable range per level customizations completely");
+            modEnabled = Config.Bind<bool>("1 - General", "a. Mod enabled", true, "Enable or disable the mod completely");
+            logsEnabled = Config.Bind<bool>("1 - General", "b. Logs enabled", false, "Enable or disable logs completely");
+            rangeEnabled = Config.Bind<bool>("1 - General", "c. Base range mods enabled", true, "Enable base or disable range customizations completely");
+            rangeLvlEnabled = Config.Bind<bool>("1 - General", "d. Range per level mods enabled", true, "Enable or disable range per level customizations completely");
 
-            globalEnabled = Config.Bind<bool>("2 - Global", "a. Global settings", false, "Enable or disable settings that apply to all stations");
+            globalEnabled = Config.Bind<bool>("2 - Global", "a. Global settings enabled", false, "Enable or disable settings that apply to all stations");
             globalRange = Config.Bind<float>("2 - Global", "b. Global base range", 30f, "Base range to apply to all stations");
             globalRangePerLevel = Config.Bind<float>("2 - Global", "c. Global range per level", 6f, "Range per level to apply to all stations");
 
-            workbenchEnabled = Config.Bind<bool>("3 - Workbench", "a. Modifications", true, "Enable or disable workbench modifications completely");
+            workbenchEnabled = Config.Bind<bool>("3 - Workbench", "a. Enabled", true, "Enable or disable workbench modifications completely");
             workbenchRange = Config.Bind<float>("3 - Workbench", "b. Base range", 30f, "Workbench base range");
             workbenchRangeLvl = Config.Bind<float>("3 - Workbench", "c. Range per level", 6f, "Workbench range per level");
 
-            forgeEnabled = Config.Bind<bool>("4 - Forge", "a. Modifications", true, "Enable or disable forge modifications completely");
+            forgeEnabled = Config.Bind<bool>("4 - Forge", "a. Enabled", true, "Enable or disable forge modifications completely");
             forgeRange = Config.Bind<float>("4 - Forge", "b. Base range", 30f, "Forge base range");
             forgeRangeLvl = Config.Bind<float>("4 - Forge", "c. Range per level", 4f, "Forge range per level");
 
-            stonecutterEnabled = Config.Bind<bool>("5 - Stonecutter", "a. Modifications", true, "Enable or disable stonecutter modifications completely");
+            stonecutterEnabled = Config.Bind<bool>("5 - Stonecutter", "a. Enabled", true, "Enable or disable stonecutter modifications completely");
             stonecutterRange = Config.Bind<float>("5 - Stonecutter", "b. Base range", 54f, "Stonecutter base range");
 
-            blackforgeEnabled = Config.Bind<bool>("6 - Blackforge", "a. Modifications", true, "Enable or disable blackforge modifications completely");
+            blackforgeEnabled = Config.Bind<bool>("6 - Blackforge", "a. Enabled", true, "Enable or disable blackforge modifications completely");
             blackforgeRange = Config.Bind<float>("6 - Blackforge", "b. Base range", 40f, "Blackforge base range");
             blackforgeRangeLvl = Config.Bind<float>("6 - Blackforge", "c. Range per level", 7f, "Blackforge range per level");
 
-            artisanEnabled = Config.Bind<bool>("7 - Artisan Table", "a. Modifications", true, "Enable or disable artisan table modifications completely");
+            artisanEnabled = Config.Bind<bool>("7 - Artisan Table", "a. Enabled", true, "Enable or disable artisan table modifications completely");
             artisanTableRange = Config.Bind<float>("7 - Artisan Table", "b. Base range", 54f, "Artisan table base range");
 
-            galdrEnabled = Config.Bind<bool>("8 - Galdr Table", "a. Modifications", true, "Enable or disable galdr table modifications completely");
+            galdrEnabled = Config.Bind<bool>("8 - Galdr Table", "a. Enabled", true, "Enable or disable galdr table modifications completely");
             galdrTableRange = Config.Bind<float>("8 - Galdr Table", "b. Base range", 40f, "Galdr table base range");
             galdrTableRangeLvl = Config.Bind<float>("8 - Galdr Table", "c. Range per level", 7f, "Galdr table range per level");
 
-            extMaxDistEnabled = Config.Bind<bool>("9 - Upgrades build distance", "a. Modifications", true, "Enable or disable station upgrades maximum distance customizations completely");
+            extMaxDistEnabled = Config.Bind<bool>("9 - Upgrades build distance", "a. Enabled", true, "Enable or disable station upgrades maximum distance customizations completely");
             extMaxDist = Config.Bind<float>("9 - Upgrades build distance", "b. Max Distance", 15f, "Maximum distance multiplier for station upgrades");
 
             if (!modEnabled.Value) return;
